d55d4bdf6efe061934b260b00337b559bd09d59c
Micael Silva <micael.silva@accountfy.com>
2023-05-18 21:17:16 -0300

    funcionano

--- code/backend/src/main.py
+++ code/backend/src/main.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 from algorithm.marques_algorithm import summarize
-import tempfile
-import PyPDF2
 
 app = Flask(__name__)
 CORS(app)
@@ -13,28 +12,6 @@ def summarize_text():
     data = request.get_json()
     text = data['text']
     num_sentences = int(data['num_sentences'])
-    summary = summarize(text, num_sentences)
-    return jsonify(summary=summary)
-
-
-@app.route('/upload', methods=['POST'])
-def upload_pdf():
-    file = request.files['file']
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    file.save(temp_file.name)
-
-    with open(temp_file.name, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        text = ''
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-
-    num_sentences = request.form.get('num_sentences')
-    if num_sentences is None:
-        num_sentences = 1
-    else:
-        num_sentences = int(num_sentences)
-
     summary = summarize(text, num_sentences)
     return jsonify(summary=summary)
 
