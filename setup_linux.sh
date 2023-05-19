8fc4c250caf73a3cd259a42ca7e8a7c6ccaa08ef
Micael Marques <micasmarques1132@gmail.com>
2023-05-18 21:33:01 -0300

    criado scripts

---
+++ setup_linux.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Atualiza a lista de pacotes
+sudo apt-get update -y
+
+# Verifica se o python3.10 está instalado
+if ! command -v python3.10 &> /dev/null
+then
+    echo "Python 3.10 não está instalado. Instalando agora..."
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt-get update -y
+    sudo apt-get install python3.10 -y
+else
+    echo "Python 3.10 já está instalado."
+fi
+
+# Instala o pip para o Python 3.10
+sudo apt-get install python3.10-distutils -y
+curl https://bootstrap.pypa.io/get-pip.py | sudo python3.10
+
+# Instala os pacotes Python
+python3.10 -m pip install nltk flask flask_cors PyPDF2
+
+# Verifica se o Node.js e npm estão instalados
+if ! command -v node &> /dev/null
+then
+    echo "Node.js não está instalado. Instalando agora..."
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+else
+    echo "Node.js já está instalado."
+fi
+
+# Muda para o diretório do frontend e roda o npm install
+cd code/frontend
+npm install
+
+# Volta para o diretório principal
+cd ../..
+
+# Roda o programa principal Python
+cd code/backend/src
+nohup python3.10 main.py &
+
+# Muda para o diretório do frontend e roda o npm start
+cd ../../frontend
+npm start
