4646e569a8d3ce17b160dcebe0df1f9ac7af9961
Micael Silva <micael.silva@accountfy.com>
2023-05-19 17:24:35 -0300

    melhorando o forms

--- code/frontend/src/components/Summary.js
+++ code/frontend/src/components/Summary.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import LanguageSwitcher from './LanguageSwitcher';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
 
 function Summary() {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const [text, setText] = useState('');
     const [numSentences, setNumSentences] = useState(1);
     const [summary, setSummary] = useState('');
@@ -62,57 +62,91 @@ function Summary() {
                     {t('textSummarization')}
                 </Typography>
             </Tooltip>
-            <Tooltip title={t('inputTooltip')}>
-                <TextField
-                    variant="outlined"
-                    multiline
-                    rows={4}
-                    fullWidth
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    label={t('enterText')}
-                />
-            </Tooltip>
-            <Tooltip title={t('numberTooltip')}>
-                <TextField
-                    variant="outlined"
-                    type="number"
-                    value={numSentences}
-                    onChange={(e) => setNumSentences(e.target.value)}
-                    label={t('numberOfSentences')}
-                />
-            </Tooltip>
-            <Tooltip title={t('buttonTooltip')}>
-                <Button variant="contained" onClick={handleClick}>
-                    {t('summarize')}
-                </Button>
-            </Tooltip>
             <Box
                 sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'stretch',
+                    justifyContent: 'space-between',
+                    gap: 2,
                     width: '100%',
-                    maxWidth: '800px',
-                    margin: '20px auto',
-                    padding: '20px',
-                    border: '1px solid #ccc',
-                    borderRadius: '4px',
-                    textAlign: 'center'
                 }}
             >
-                <Typography variant="h6" component="div" gutterBottom>
-                    {t('summary')}
-                </Typography>
-                <Typography variant="body1" component="pre" style={{whiteSpace: 'pre-wrap', textAlign: 'justify', alignItems: 'flex-start'}}>{summary}</Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                        width: '60%', // adjust this as needed
+                    }}
+                >
+                    <Tooltip title={t('inputTooltip')}>
+                        <TextField
+                            variant="outlined"
+                            multiline
+                            fullWidth
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                            label={t('enterText')}
+                            maxRows={10}
+                            style={{overflow: 'auto'}}
+                        />
+                    </Tooltip>
+                    <Tooltip title={t('numberTooltip')}>
+                        <TextField
+                            variant="outlined"
+                            type="number"
+                            value={numSentences}
+                            onChange={(e) => setNumSentences(e.target.value)}
+                            label={t('numberOfSentences')}
+                        />
+                    </Tooltip>
+                    {text && (
+                        <Button variant="contained" onClick={handleClearText}>
+                            {t('resetText')}
+                        </Button>
+                    )}
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        gap: 2,
+                    }}
+                >
+                    <Tooltip title={t('buttonTooltip')}>
+                        <Button variant="contained" onClick={handleClick}>
+                            {t('summarize')}
+                        </Button>
+                    </Tooltip>
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                        width: '60%', // adjust this as needed
+                        margin: '20px auto',
+                        padding: '20px',
+                        border: '1px solid #ccc',
+                        borderRadius: '4px',
+                        textAlign: 'center'
+                    }}
+                >
+                    <Typography variant="h6" component="div" gutterBottom>
+                        {t('summary')}
+                    </Typography>
+                    <Box sx={{overflow: 'auto', maxHeight: '180px'}}>
+                        <Typography variant="body1" component="pre" style={{whiteSpace: 'pre-wrap', textAlign: 'justify'}}>{summary}</Typography>
+                    </Box>
+                    {summary && (
+                        <Button variant="contained" onClick={handleClearSummary}>
+                            {t('resetSummary')}
+                        </Button>
+                    )}
+                </Box>
             </Box>
-            {summary && (
-                <Button variant="contained" onClick={handleClearSummary}>
-                    {t('resetSummary')}
-                </Button>
-            )}
-            {text && (
-                <Button variant="contained" onClick={handleClearText}>
-                    {t('resetText')}
-                </Button>
-            )}
         </Box>
     );
 }
