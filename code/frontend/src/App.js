a96685aeec6214d6647d8ef18bfe6b4bcdd5f917
Micael Silva <micael.silva@accountfy.com>
2023-05-20 18:22:33 -0300

    adicionado git, linkedin, insta e email

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -3,7 +3,8 @@ import {Link, BrowserRouter as Router} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
 import {Navbar, Nav} from 'react-bootstrap';
 import Summary from './components/Summary';
-import './styles/App.css'; // se você tem um arquivo CSS para estilização global
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa';
+import './styles/App.css';
 
 function App() {
     const {t} = useTranslation();
@@ -31,6 +32,22 @@ function App() {
                     </Navbar.Collapse>
                 </Navbar>
                 <Summary/>
+                <Navbar fixed="bottom" bg="light">
+                    <Nav className="mx-auto">
+                        <Nav.Link href="https://github.com/micasmarques" target="_blank">
+                            <FaGithub />
+                        </Nav.Link>
+                        <Nav.Link href="https://www.linkedin.com/in/micael-marques/" target="_blank">
+                            <FaLinkedin />
+                        </Nav.Link>
+                        <Nav.Link href="https://www.instagram.com/micasmarques/" target="_blank">
+                            <FaInstagram />
+                        </Nav.Link>
+                        <Nav.Link href="mailto:micasmarques1132@gmail.com">
+                            <FaEnvelope />
+                        </Nav.Link>
+                    </Nav>
+                </Navbar>
             </div>
         </Router>
     );
