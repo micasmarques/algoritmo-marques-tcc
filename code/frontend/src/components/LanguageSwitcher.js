0668eca71d558a9c65fad96614d6515d7e2db3c6
Micael Silva <micael.silva@accountfy.com>
2023-05-20 15:12:50 -0300

    traducao adicionada

--- code/frontend/src/components/LanguageSwitcher.js
+++ code/frontend/src/components/LanguageSwitcher.js
@@ -2,27 +2,30 @@ import React from 'react';
 import {useTranslation} from 'react-i18next';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import {useTheme} from '@mui/material/styles';
 
 function LanguageSwitcher() {
-  const {i18n} = useTranslation();
+    const {i18n} = useTranslation();
+    const theme = useTheme();
 
-  const handleLanguageChange = (event) => {
-    i18n.changeLanguage(event.target.checked ? 'en' : 'pt');
-  };
+    const handleLanguageChange = (event) => {
+        i18n.changeLanguage(event.target.checked ? 'en' : 'pt');
+    };
 
-  return (
-    <FormControlLabel
-      control={
-        <Switch
-          checked={i18n.language === 'en'}
-          onChange={handleLanguageChange}
-          name="languageSwitch"
-          color="default"
+    return (
+        <FormControlLabel
+            control={
+                <Switch
+                    checked={i18n.language === 'en'}
+                    onChange={handleLanguageChange}
+                    name="languageSwitch"
+                    color="default"
+                />
+            }
+            label={<span
+                style={{color: theme.palette.text.primary}}>{i18n.language === 'en' ? 'English' : 'Português'}</span>}
         />
-      }
-      label={i18n.language === 'en' ? 'English' : 'Português'}
-    />
-  );
+    );
 }
 
 export default LanguageSwitcher;
