3058b91b9e4bea5f06c66e28f8f94ce35cc67cd0
Micael Silva <micael.silva@accountfy.com>
2023-05-20 23:23:49 -0300

    icone do git

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
-import {useTranslation} from 'react-i18next';
-import {Navbar, Nav} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Navbar, Nav } from 'react-bootstrap';
 import Summary from './components/Summary';
 import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa';
 import './styles/App.css';
 
 function App() {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const [darkMode, setDarkMode] = useState(false);
 
     useEffect(() => {
@@ -23,15 +23,16 @@ function App() {
             <div className="App">
                 <Navbar bg="light" expand="lg">
                     <Navbar.Brand>{t('Algoritmo de Marques')}</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbar"/>
+                    <Navbar.Toggle aria-controls="navbar" />
                     <Navbar.Collapse id="navbar">
                         <Nav className="mr-auto">
-                            <Link to="https://github.com/micasmarques/algoritmo-marques-tcc"
-                                  target="_blank">{t('Sobre')}</Link>
+                            <Link to="https://github.com/micasmarques/algoritmo-marques-tcc" target="_blank">
+                                {t('Sobre')} <FaGithub />
+                            </Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-                <Summary/>
+                <Summary />
                 <Navbar fixed="bottom" bg="light">
                     <Nav className="mx-auto">
                         <Nav.Link href="https://github.com/micasmarques" target="_blank">
