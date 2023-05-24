569355de688463fc8ec224c4130b58f93d5286fc
Micael Silva <micael.silva@accountfy.com>
2023-05-24 00:10:15 -0300

    popup adicionado

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,27 +1,31 @@
-import React, {useState, useEffect} from 'react';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
-import {useTranslation} from 'react-i18next';
-import {Navbar, Nav} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Navbar, Nav, Modal, Button, Form } from 'react-bootstrap';
 import Summary from './components/Summary';
-import {FaGithub, FaLinkedin, FaInstagram, FaEnvelope} from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa';
 import './styles/App.css';
 
 function App() {
-    const {t} = useTranslation();
-    const [darkMode] = useState(false);
+    const { t } = useTranslation();
+    const [darkMode, setDarkMode] = useState(false);
+    const [showModal, setShowModal] = useState(true);
+
+    const handleClose = () => setShowModal(false);
+    const handleDarkModeChange = (e) => setDarkMode(e.target.checked);
 
     useEffect(() => {
         if (darkMode) {
-            document.body.classList.add('dark-mode');
+            document.documentElement.classList.add('dark-mode');
         } else {
-            document.body.classList.remove('dark-mode');
+            document.documentElement.classList.remove('dark-mode');
         }
     }, [darkMode]);
 
     return (
         <Router>
             <div className="App">
-                <Navbar bg="light" expand="lg" className="top-navbar">
+                <Navbar bg={darkMode ? "dark" : "light"} variant={darkMode ? "dark" : "light"} expand="lg" className="top-navbar">
                     <Navbar.Brand className="navbar-center">
                         {t('Algoritmo de Marques')}
                     </Navbar.Brand>
@@ -35,10 +39,19 @@ function App() {
                                 {t('Sobre')} <FaGithub/>
                             </Nav.Link>
                         </Nav>
+                        <Form inline>
+                            <Form.Check
+                                type="switch"
+                                id="custom-switch"
+                                label="Dark Mode"
+                                checked={darkMode}
+                                onChange={handleDarkModeChange}
+                            />
+                        </Form>
                     </Navbar.Collapse>
                 </Navbar>
                 <Summary/>
-                <Navbar fixed="bottom" bg="light" className="bottom-navbar">
+                <Navbar fixed="bottom" bg={darkMode ? "dark" : "light"} variant={darkMode ? "dark" : "light"} className="bottom-navbar">
                     <Nav className="mx-auto">
                         <Nav.Link href="https://github.com/micasmarques" target="_blank">
                             <FaGithub/>
@@ -60,9 +73,24 @@ function App() {
                         </Nav.Link>
                     </Nav>
                 </Navbar>
+
+                <Modal show={showModal} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Aviso</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Se você está tendo dificuldades com o layout da página, tente usá-lo em um computador e diminuir o zoom da página.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleClose}>
+                            Entendi
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
             </div>
         </Router>
     );
 }
 
-export default App;
+export default App
